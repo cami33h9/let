0546a7551ea0abf36a97e5834e25cadce3e97320
cami33h9 <cami33h9@stud.kea.dk>
2021-10-15 14:19:17 +0200

    fredag

--- javascript.js
+++ javascript.js
@@ -1,1 +1,36 @@
+gsap.from(".ani", {
+  y: 100,
+  opacity: 0,
+  stagger: 0.3,
+  scrollTrigger: {
+    trigger: ".ani",
+    start: "top 80%",
+    end: "top 20%",
+    toggleActions: "play none none reverse",
+    // markers: true,
+  },
+});
+gsap.from(".ani2", {
+  x: 100,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".ani2",
+    start: "top 50%",
+    end: "top 20%",
+    toggleActions: "play none none reverse",
+    // markers: true,
+  },
+});
 
+gsap.from(".ani3", {
+  x: 100,
+  opacity: 0,
+  stagger: 0.3,
+  scrollTrigger: {
+    trigger: ".ani3",
+    start: "top 50%",
+    end: "top 20%",
+    toggleActions: "play none none reverse",
+    // markers: true,
+  },
+});
